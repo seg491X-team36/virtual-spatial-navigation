1a09b51d477457b19efb4d466e19b0119bdf5404
Daniel <danielh839@gmail.com>
2023-02-20 11:27:58 -0500

    add context parameter to ExperimentService methods

--- vsn-backend/features/experiment/experiment_handlers.go
+++ vsn-backend/features/experiment/experiment_handlers.go
@@ -11,11 +11,11 @@ import (
 
 // ExperimentService interface required by ExperimentHandlers
 type ExperimentService interface {
-	Pending(userId uuid.UUID) pendingExperimentsResponse
-	StartExperiment(userId, experimentId uuid.UUID) startExperimentResponse
-	StartRound(userId uuid.UUID) (*model.ExperimentStatus, error)
-	StopRound(userId uuid.UUID) (*model.ExperimentStatus, error)
-	Record(userId uuid.UUID, request recordDataRequest) error
+	Pending(ctx context.Context, userId uuid.UUID) pendingExperimentsResponse
+	StartExperiment(ctx context.Context, userId, experimentId uuid.UUID) startExperimentResponse
+	StartRound(ctx context.Context, userId uuid.UUID) (*model.ExperimentStatus, error)
+	StopRound(ctx context.Context, userId uuid.UUID) (*model.ExperimentStatus, error)
+	Record(ctx context.Context, userId uuid.UUID, request recordDataRequest) error
 }
 
 type ExperimentHandlers struct {
@@ -25,21 +25,21 @@ type ExperimentHandlers struct {
 func (e *ExperimentHandlers) Pending() http.HandlerFunc {
 	return postRequestWrapper(func(ctx context.Context, _ pendingExperimentsRequest) pendingExperimentsResponse {
 		token, _ := security.AuthToken(ctx)
-		return e.ExperimentService.Pending(token.UserId) // experiment service pending method
+		return e.ExperimentService.Pending(ctx, token.UserId) // experiment service pending method
 	})
 }
 
 func (e *ExperimentHandlers) StartExperiment() http.HandlerFunc {
 	return postRequestWrapper(func(ctx context.Context, req startExperimentRequest) startExperimentResponse {
 		token, _ := security.AuthToken(ctx)
-		return e.ExperimentService.StartExperiment(token.UserId, req.ExperimentId) // experiment service start experiment method
+		return e.ExperimentService.StartExperiment(ctx, token.UserId, req.ExperimentId) // experiment service start experiment method
 	})
 }
 
 func (e *ExperimentHandlers) StartRound() http.HandlerFunc {
 	return postRequestWrapper(func(ctx context.Context, req startRoundRequest) startRoundResponse {
 		token, _ := security.AuthToken(ctx)
-		status, err := e.ExperimentService.StartRound(token.UserId) // experiment service start round method
+		status, err := e.ExperimentService.StartRound(ctx, token.UserId) // experiment service start round method
 
 		return startRoundResponse{
 			Status: status,
@@ -51,7 +51,7 @@ func (e *ExperimentHandlers) StartRound() http.HandlerFunc {
 func (e *ExperimentHandlers) StopRound() http.HandlerFunc {
 	return postRequestWrapper(func(ctx context.Context, req startRoundRequest) stopRoundResponse {
 		token, _ := security.AuthToken(ctx)
-		status, err := e.ExperimentService.StopRound(token.UserId) // experiment service stop round method
+		status, err := e.ExperimentService.StopRound(ctx, token.UserId) // experiment service stop round method
 
 		return stopRoundResponse{
 			Status: status,
@@ -63,7 +63,7 @@ func (e *ExperimentHandlers) StopRound() http.HandlerFunc {
 func (e *ExperimentHandlers) Record() http.HandlerFunc {
 	return postRequestWrapper(func(ctx context.Context, req recordDataRequest) recordDataResponse {
 		token, _ := security.AuthToken(ctx)
-		err := e.ExperimentService.Record(token.UserId, req) // experiment service record method
+		err := e.ExperimentService.Record(ctx, token.UserId, req) // experiment service record method
 
 		return recordDataResponse{
 			Error: errWrapper(err),
