9d84e3ab7cba63b25daea1ca2ce004b6451c9213
Daniel <danielh839@gmail.com>
2023-02-27 20:02:00 -0500

    implement user resolvers

--- vsn-backend/features/resolvers/user.go
+++ vsn-backend/features/resolvers/user.go
@@ -2,18 +2,24 @@ package resolvers
 
 import (
 	"context"
-	"errors"
 
 	"github.com/seg491X-team36/vsn-backend/domain/model"
+	"github.com/seg491X-team36/vsn-backend/domain/repository"
 )
 
 type UserResolvers struct {
+	repository.InviteRepository
+	repository.ExperimentResultRepository
 }
 
-func (r *UserResolvers) Invites(ctx context.Context, obj *model.User) ([]model.Invite, error) {
-	return []model.Invite{}, errors.New("not implemented")
+func (r *UserResolvers) Invites(ctx context.Context, user *model.User) ([]model.Invite, error) {
+	// call the invite repository
+	invites := r.InviteRepository.GetPendingInvitesByUserId(ctx, user.Id)
+	return invites, nil
 }
 
-func (r *UserResolvers) Results(ctx context.Context, obj *model.User) ([]model.ExperimentResult, error) {
-	return []model.ExperimentResult{}, errors.New("not implemented")
+func (r *UserResolvers) Results(ctx context.Context, user *model.User) ([]model.ExperimentResult, error) {
+	// call the experiment repository
+	results := r.ExperimentResultRepository.GetExperimentResultsByUserId(ctx, user.Id)
+	return results, nil
 }
