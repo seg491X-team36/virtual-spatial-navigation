230cd6020e1fa9718548ad9d6e70e4d2f53db3dd
Cadence Yeung <cadence.yeung@gmail.com>
2023-03-06 15:22:14 -0500

    first commit to home page

--- vsn-frontend/src/pages/Home.js
+++ vsn-frontend/src/pages/Home.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
 
 import 'semantic-ui-css/semantic.min.css'
+import { Button, ButtonGroup } from 'semantic-ui-react';
 
 const Home = () => {
     return(
         <>
-        <h1>Home</h1>
+        <h1>Welcome!</h1>
+        <h2>What would you like to do?</h2>
+        <ButtonGroup>
+            <Button>View Experiments</Button>
+            <Button>View Participants</Button>
+            <Button>Rejected Users</Button>
+            <Button>View Sign Ups</Button>
+        </ButtonGroup>
         </>
     )
 };
