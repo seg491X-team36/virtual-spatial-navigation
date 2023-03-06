e2abe23a05b2f6976f7f6e1db100f987889aaa48
Cadence Yeung <cadence.yeung@gmail.com>
2023-03-06 16:15:58 -0500

    basic table for sign ups

--- vsn-frontend/src/pages/ViewSignUps.js
+++ vsn-frontend/src/pages/ViewSignUps.js
@@ -3,9 +3,33 @@ import 'semantic-ui-css/semantic.min.css'
 import NavBar from '../components/NavBar';
 
 const ViewSignUps = () => {
-    return(
+    return (
         <>
-        <h1>Sign Ups</h1>
+            <h1>Sign Ups</h1>
+            <table class="ui celled table">
+                <thead>
+                    <tr><th>Email</th>
+                        <th></th>
+                        <th></th>
+                    </tr></thead>
+                <tbody>
+                    <tr>
+                        <td data-label="Email">cdame453@uottawa.ca</td>
+                        <td data-label="Accept">Accept</td>
+                        <td data-label="Reject">Reject</td>
+                    </tr>
+                    <tr>
+                        <td data-label="Email">ascfb435@uottawa.ca</td>
+                        <td data-label="Accept">Accept</td>
+                        <td data-label="Reject">Reject</td>
+                    </tr>
+                    <tr>
+                        <td data-label="Email">ngdfg765@uottawa.ca</td>
+                        <td data-label="Accept">Accept</td>
+                        <td data-label="Reject">Reject</td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 };
