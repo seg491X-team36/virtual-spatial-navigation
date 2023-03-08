6c086382eb6589e44193c7ba949e900e9247fa0c
Cadence Yeung <cadence.yeung@gmail.com>
2023-03-08 14:48:26 -0500

    redesign of home page skeleton

--- vsn-frontend/src/pages/Home.js
+++ vsn-frontend/src/pages/Home.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 
 import 'semantic-ui-css/semantic.min.css'
-import { Button, ButtonGroup } from 'semantic-ui-react';
+// import { Button, ButtonGroup } from 'semantic-ui-react';
 
 const Home = () => {
     return (
         <>
-            <h1>Welcome!</h1>
-            <h2>What would you like to do?</h2>
-            <button class="ui button" tabindex="0">View Experiments</button>
-            <button class="ui button" tabindex="0">View Participants</button>
-            <button class="ui button" tabindex="0">Rejected Users</button>
-            <button class="ui button" tabindex="0">View Sign Ups</button>
+            <h1 style={{textAlign:'center'}}>Virtual Space Navigation</h1>
+            <h2 style={{textAlign:'center'}}>Welcome, Admin!</h2>
         </>
     )
 };
